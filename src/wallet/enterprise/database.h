14543ca26235de93efd45e2533516926d35452c3
Pierre Rochard <pierre@rochard.org>
2017-10-22 20:12:54 -0400

    add command line args for db connection

--- src/wallet/enterprise/database.h
+++ src/wallet/enterprise/database.h
@@ -4,6 +4,8 @@
 #include <odb/database.hxx>
 #include <odb/pgsql/database.hxx>
 
+#include "util.h"
+
 inline std::auto_ptr <odb::database>
 create_enterprise_database() {
     using namespace std;
@@ -11,11 +13,11 @@ create_enterprise_database() {
 
     auto_ptr <odb::database> enterprise_database(
             new odb::pgsql::database(
-                    "test_user",
-                    "test_password",
-                    "test_database",
-                    "localhost",
-                    "5432"
+                    gArgs.GetArg("-enterprise_user", "test_user"),
+                    gArgs.GetArg("-enterprise_password", "test_password"),
+                    gArgs.GetArg("-enterprise_database", "test_database"),
+                    gArgs.GetArg("-enterprise_host", "localhost"),
+                    gArgs.GetArg("-enterprise_port", "5432")
             ));
 
     return enterprise_database;
