7155576584cd89e2bd5aabfc0dac2245273847bc
Pierre Rochard <pierre.rochard@axial.net>
2017-12-25 15:55:33 -0500

    use environment variables

--- src/wallet/enterprise/database.h
+++ src/wallet/enterprise/database.h
@@ -8,16 +8,14 @@
 
 inline std::auto_ptr <odb::database>
 create_enterprise_database() {
-    using namespace std;
-    using namespace odb::core;
 
-    auto_ptr <odb::database> enterprise_database(
+    std::auto_ptr <odb::database> enterprise_database(
             new odb::pgsql::database(
-                    gArgs.GetArg("-enterprise_user", "test_user"),
-                    gArgs.GetArg("-enterprise_password", "test_password"),
-                    gArgs.GetArg("-enterprise_database", "test_database"),
-                    gArgs.GetArg("-enterprise_host", "localhost"),
-                    gArgs.GetArg("-enterprise_port", "5432")
+                    gArgs.GetArg("-enterprise_user", std::getenv("ENTERPRISE_PGUSER")),
+                    gArgs.GetArg("-enterprise_password", std::getenv("ENTERPRISE_PGPASSWORD")),
+                    gArgs.GetArg("-enterprise_database", std::getenv("ENTERPRISE_PGDATABASE")),
+                    gArgs.GetArg("-enterprise_host", std::getenv("ENTERPRISE_PGHOST")),
+                    gArgs.GetArg("-enterprise_port", std::getenv("ENTERPRISE_PGPORT"))
             ));
 
     return enterprise_database;
