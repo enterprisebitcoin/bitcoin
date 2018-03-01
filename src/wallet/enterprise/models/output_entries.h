b0f5e9966ae6d8f3a9ee82c2c05bce109141646a
Pierre Rochard <pierre@rochard.org>
2018-03-01 01:35:05 -0500

    refactor outputs table

--- src/wallet/enterprise/models/output_entries.h
+++ src/wallet/enterprise/models/output_entries.h
@@ -10,34 +10,66 @@
 
 #pragma db object
 
+/**
+ *
+ * A combination of a CTxOut and the CTxIn that spends it, if it exists
+ *
+ **/
+
 class eOutputEntries {
 public:
     eOutputEntries(
             unsigned int output_etransaction_id,
             unsigned int output_vector,
             uint8_t is_output_mine,
+            int64_t n_value,
+            std::string destination,
+            std::string script_pub_key,
+
             unsigned int input_etransaction_id,
             unsigned int input_vector,
-            int64_t amount,
-            std::string destination
+            std::string script_sig,
+            std::string script_witness,
+            std::string n_sequence
     ) : output_etransaction_id(output_etransaction_id),
         output_vector(output_vector),
-        output_is_mine(output_is_mine),
+        is_output_mine(is_output_mine),
+        n_value(n_value),
+        destination(destination),
+        script_pub_key(script_pub_key),
+
         input_etransaction_id(input_etransaction_id),
         input_vector(input_vector),
-        amount(amount),
-        destination(destination) {};
+        script_sig(script_sig),
+        script_witness(script_witness),
+        n_sequence(n_sequence) {};
 
+    #pragma db id auto
+    unsigned int id;
+
+    /**
+     *  CTxOut
+     **/
     unsigned int output_etransaction_id;
     unsigned int output_vector;
-    uint8_t output_is_mine;
-    unsigned int input_etransaction_id;
-    unsigned int input_vector;
-    int64_t amount;
+    uint8_t is_output_mine;
+    int64_t n_value;
     std::string destination;
+    std::string script_pub_key;
 
-#pragma db id auto
-    unsigned int id;
+    /**
+     *  CTxIn
+     **/
+    #pragma db null
+    unsigned int input_etransaction_id;
+    #pragma db null
+    unsigned int input_vector;
+    #pragma db null
+    std::string script_sig;
+    #pragma db null
+    std::string script_witness;
+    #pragma db null
+    std::string n_sequence;
 
 private:
     friend class odb::access;
