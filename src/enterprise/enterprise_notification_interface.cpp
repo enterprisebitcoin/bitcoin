a21c91c805ada2bb4f3d99f5a70b14fcc093fa74
Pierre Rochard <pierre@rochard.org>
2018-08-23 12:36:46 -0400

    Enterprise: Add a notification interface for inserting blocks into the SQL database

---
+++ src/enterprise/enterprise_notification_interface.cpp
@@ -0,0 +1,38 @@
+#include <primitives/block.h>
+#include <chain.h>
+#include <util.h>
+
+#include <enterprise/enterprise_notification_interface.h>
+#include <enterprise/block_to_sql.h>
+
+void EnterpriseNotificationInterface::UpdatedBlockTip(const CBlockIndex *pindexNew,
+                                                      const CBlockIndex *pindexFork,
+                                                      bool fInitialDownload) {
+    LogPrintf("UpdatedBlockTip pindexNew %s\n", pindexNew->ToString());
+    if (pindexFork) {
+        LogPrintf("UpdatedBlockTip pindexFork %s\n", pindexFork->ToString());
+    }
+}
+
+void EnterpriseNotificationInterface::TransactionAddedToMempool(const CTransactionRef &ptxn) {
+    ;
+//    LogPrintf("TransactionAddedToMempool %s", ptxn->ToString());
+}
+
+void EnterpriseNotificationInterface::TransactionRemovedFromMempool(const CTransactionRef &ptx) {
+    ;
+//    LogPrintf("TransactionRemovedFromMempool %s", ptx->ToString());
+}
+
+void EnterpriseNotificationInterface::BlockConnected(const std::shared_ptr<const CBlock> &block,
+                                                     const CBlockIndex *pindex,
+                                                     const std::vector <CTransactionRef> &txnConflicted) {
+    LogPrintf("BlockConnected %s\n", pindex->ToString());
+
+    BlockToSql block_to_sql(*pindex, *block);
+    block_to_sql.InsertBlock();
+}
+
+void EnterpriseNotificationInterface::BlockDisconnected(const std::shared_ptr<const CBlock> &block) {
+    LogPrintf("BlockDisconnected %s\n", block->GetBlockHeader().GetHash().ToString());
+}
