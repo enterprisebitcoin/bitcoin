06d5d19525080c26e2431e83d5dfce29d76e70a1
Pierre Rochard <pierre@rochard.org>
2019-08-24 19:20:25 -0400

    Add dotenv db config

--- src/enterprise/database.h
+++ src/enterprise/database.h
@@ -1,6 +1,8 @@
 #ifndef ENTERPRISE_DATABASE_H
 #define ENTERPRISE_DATABASE_H
 
+#include <enterprise/dotenv.h>
+#include <logging.h>
 #include <util/system.h>
 
 #include <odb/database.hxx>
@@ -8,18 +10,20 @@
 
 #include "util.h"
 
-inline std::auto_ptr <odb::database>
-create_enterprise_database() {
+using namespace dotenv;
 
-    std::auto_ptr <odb::database> enterprise_database(
-            new odb::pgsql::database(
-                    gArgs.GetArg("-enterprise_user", std::getenv("ENTERPRISE_PGUSER")),
-                    gArgs.GetArg("-enterprise_password", std::getenv("ENTERPRISE_PGPASSWORD")),
-                    gArgs.GetArg("-enterprise_database", std::getenv("ENTERPRISE_PGDATABASE")),
-                    gArgs.GetArg("-enterprise_host", std::getenv("ENTERPRISE_PGHOST")),
-                    gArgs.GetArg("-enterprise_port", std::getenv("ENTERPRISE_PGPORT"))
-            ));
-
+inline std::auto_ptr<odb::database>
+create_enterprise_database()
+{
+    auto& dotenv = env;
+    dotenv.config();
+    std::auto_ptr<odb::database> enterprise_database(
+        new odb::pgsql::database(
+            dotenv["PGUSER"],
+            dotenv["PGPASSWORD"],
+            dotenv["PGDB"],
+            dotenv["PGHOST"],
+            dotenv["PGPORT"]));
     return enterprise_database;
 }
 
--- src/enterprise/models/blocks.h
+++ src/enterprise/models/blocks.h
@@ -16,32 +16,32 @@
  *
  **/
 
-class eBlocks {
+class eBlocks
+{
 public:
     eBlocks(
-            const std::string hash,
-            const std::string merkle_root,
-            const int64_t time,
-            const int64_t height,
-            const unsigned int transactions_count,
-            const uint32_t version,
-            const uint32_t status,
-            const uint32_t bits,
-            const uint64_t nonce
-    )
-            :
-            hash(hash),
-            merkle_root(merkle_root),
-            time(time),
-            height(height),
-            transactions_count(transactions_count),
-            version(version),
-            status(status),
-            bits(bits),
-            nonce(nonce) {
+        const std::string hash,
+        const std::string merkle_root,
+        const int64_t time,
+        const int64_t height,
+        const unsigned int transactions_count,
+        const uint32_t version,
+        const uint32_t status,
+        const uint32_t bits,
+        const uint64_t nonce)
+        : hash(hash),
+          merkle_root(merkle_root),
+          time(time),
+          height(height),
+          transactions_count(transactions_count),
+          version(version),
+          status(status),
+          bits(bits),
+          nonce(nonce)
+    {
     }
 
-    #pragma db id auto
+#pragma db id auto
     unsigned int id;
 
     std::string hash;
@@ -59,7 +59,6 @@ private:
     friend class odb::access;
 
     eBlocks() {}
-
 };
 
 #endif //ENTERPRISE_BLOCKS_H
